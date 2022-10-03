91ee2740445ed9d3087c94b16683e71d7a8caa78
Kapitosha <Artpolevtsov63@mail.ru>
2022-10-03 14:40:44 +0300

    promises

--- Lab2/Programm Name/script.js
+++ Lab2/Programm Name/script.js
@@ -1,3 +1,6 @@
+const TIME_PERIOD = 5;
+const EXEC_LIMIT = 100;
+
 const fs = require("fs");
 const path = require("path");
 
@@ -14,40 +17,78 @@ const userName = userNameArr.join(" ");
 const config = path.resolve(__dirname, "../", "Programm_Data", "config.txt");
 const namesFile = path.resolve(__dirname, "Names.txt");
 
-
-fs.readFile(config, "utf8", (err, data) => {
-    if (err) console.log(err);
-    else {
-        let countExec = Number(data.split("\n")[0]);
-        countExec+=1;
-        let lastTimeExec = Number(data.split("\n")[1]);
-        const currTimeExec = new Date().getTime() / 1000;
-        const infToFile = countExec.toString() + "\n" + currTimeExec.toString();
-        if(currTimeExec - lastTimeExec > 5 && countExec < 100){
-
-        }
-        else {
-            console.log("Wait, please");
-        }
-    }
-})
-
-fs.writeFile(config, infToFile, {encoding:"utf8", flag:"w", mode:0o777},  (err)=> {
-    if(err) {console.log(err)}
-    else {
-        fs.readFile(namesFile, "utf8", (err, data)=>{
-            if(err) console.log(err);
+const readConfigAsync = async (config) => {
+    return new Promise((resolve, reject)=>{
+        fs.readFile(config, "utf8", (err, data) => {
+            if (err)
+                return reject(err.message);
             else {
-                const names = data.split("\n");
-                if(searchValue(names, userName)) console.log("Hello " + userName + " nice to see you again!");
+                let spentExec = Number(data.split("\n")[0]) + 1;
+
+                let lastTimeExec = Number(data.split("\n")[1]);
+                let currTimeExec = new Date().getTime() / 1000;
+
+                let infToFile = spentExec.toString() + "\n" + currTimeExec.toString();
+                const elapsedTime = currTimeExec - lastTimeExec;
+                const remExec = EXEC_LIMIT - spentExec;
+
+                if(spentExec > EXEC_LIMIT){
+                    return reject("LIMIT");
+                }
+
+                if(elapsedTime > TIME_PERIOD){
+                    return resolve({config: config, infToFile: infToFile, remExec: remExec});
+                }
                 else {
-                    console.log("Hello, " + userName + ", welcome!")
-                    fs.appendFile(namesFile, userName + "\n",  (err)=> {
-                        if(err) {console.log(err)};
-                    })
+                    return console.log("Waiting for " + (TIME_PERIOD - elapsedTime))
                 }
             }
         })
-    }
-});
+    })
+}
+
+const writeConfigAsync = async (obj, config, infToFile, remExec) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(obj.config, obj.infToFile, {encoding:"utf8", flag:"w", mode:0o777},  (err)=> {
+            if(err)
+                console.log(err)
+            else
+                return resolve(obj.remExec);
+        });
+    })
+}
+
+const readNamesFileAsync = async (namesFile, remExec) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(namesFile, "utf8", (err, data)=>{
+            if(err)
+                console.log(err);
+            else {
+                const names = data.split("\n");
+                if(searchValue(names, userName))
+                    console.log("Hello " + userName + " nice to see you again!" + "\n" + "Remaining execs: " + remExec);
+                else {
+                    console.log("Hello, " + userName + ", welcome!")
+                    resolve();
+                }
+            }
+        })
+    })
+}
+
+const appendNamesFileAsync = async (namesFile, userName) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(namesFile, userName + "\n",  (err)=> {
+            if(err)
+                console.log(err)
+        })
+    })
+}
+
+readConfigAsync(config)
+    .then((obj)=>writeConfigAsync(obj))
+    .then((remExec)=>readNamesFileAsync(namesFile,remExec))
+    .then((remExec)=>appendNamesFileAsync(namesFile, userName, remExec))
+
+
 
