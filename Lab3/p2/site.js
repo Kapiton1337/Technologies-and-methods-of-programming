ef4d48017dd77f6cda0e39318f981b4394fc2b04
Kapitosha <Artpolevtsov63@mail.ru>
2022-10-23 17:28:17 +0300

    added regexp

--- Lab3/p2/site.js
+++ Lab3/p2/site.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -10,10 +11,18 @@ const staticSiteOptions ={
     maxAge: 1000*60*15
 }
 
-app.use(express.static(path.resolve(__dirname, "static"), staticSiteOptions)).listen(staticSiteOptions.portNum);
-app.use(express.json())
+app.use(express.static(path.resolve(__dirname, "static"), staticSiteOptions));
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "static", "index.html"))
+}).listen(staticSiteOptions.portNum);
 
 app.post('/haha', urlencodedParser, (req, res, next)=>{
     if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
+    const userInfo = JSON.stringify(req.body).replace(/\,\"/g,"\n").replace(/[\"\}\{]/g, "").replace(/\:/g, " -- ");
+    fs.writeFile(path.resolve(__dirname, "userInfo.txt"), userInfo, "utf-8", (err)=> {
+        if(err) console.error(err);
+        else console.log("File saved")
+    })
 })
